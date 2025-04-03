35356864b4aca3befb5e07c1d4fef11c4bd3b9a6
Kouceyla <kouceyla.aliouche@ortmontreuil.fr>
2025-04-03 09:49:07 +0200

    recupmoica

--- mobile/src/modules/auth/screens/LoginScreen.tsx
+++ mobile/src/modules/auth/screens/LoginScreen.tsx
@@ -1,57 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 import { router, Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { useRef, useState } from "react";
+import {
+  Alert,
+  Button,
+  Image,
+  KeyboardAvoidingView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
+const logo = require('../resources/ort-logo.png')
 
 export const LoginScreen = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+  const [value, setValue] = useState("");
 
-  const handleSubmit = () => {
-    console.log('Nom:', name);
-    console.log('Email:', email);
-  };
+  const scrollViewRef = useRef<ScrollView>(null);
+  const passwordRef = useRef<TextInput>(null);
 
   return (
-    <View style={styles.container}>
-      <Text>Formulaire</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Nom"
-        value={name}
-        onChangeText={setName}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={email}
-        onChangeText={setEmail}
-        keyboardType="email-address"
-      />
-      <Button title="Envoyer" onPress={() =>{
-        router.push("/home");
-
-      }}  />
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView ref={scrollViewRef} style={{flex: 1}} contentContainerStyle={{ paddingBottom: 300}}>
+      <View style={styles.container}>
+        <Stack.Screen
+          options={{
+            title: "Login",
+          }}
+        />
+        <Image style={styles.image} source={logo} />
+        <TextInput
+          style={styles.input}
+          value={value}
+          onChangeText={setValue}
+          placeholder="Login"
+          onFocus={() => {
+            console.log('scroll')
+            scrollViewRef.current?.scrollToEnd()
+          }}
+          returnKeyType="next"
+          onSubmitEditing={() => {
+            passwordRef.current?.focus();
+          }}
+        />
+        <TextInput
+         ref={passwordRef}
+          style={styles.input}
+          value={value}
+          onChangeText={setValue}
+          placeholder="Password"
+          returnKeyType="send"
+        />
+        <View style={styles.button}>
+          <Button
+            title="Submit"
+            onPress={() => {
+              router.push("/home");
+            }}
+          />
+        </View>
+      </View>
+    </ScrollView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
+    backgroundColor: "#fff",
+    padding: 16,
+    gap: 16,
   },
   input: {
-    width: '100%',
     borderWidth: 1,
-    borderColor: '#ccc',
-    padding: 10,
-    marginVertical: 10,
-    borderRadius: 5,
+    borderRadius: 10,
+    padding: 16,
+  },
+  image: {
+    alignSelf: "center",
+  },
+  button: {
+    borderWidth: 1,
+    borderRadius: 50,
+    padding: 16,
   },
 });
